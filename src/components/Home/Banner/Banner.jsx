54d6dfe9a08757d71b2f8ea8de5edb75ccfb91f8
shrutiso05 <shristisoni051199@gmail.com>
2024-01-05 15:35:05 +0530

    set_up homepage

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,21 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                        Convalis interum j;lgkd;kgkkgoprkoperkpgkdfl;kgdkf;gk
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className= "banner-img"src={BannerImg} alt="" />
+        </div>
+    </div>;
 };
 
 export default Banner;
