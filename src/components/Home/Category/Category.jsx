0714e76780588597db24953ab8690b4b18eb0845
shrutiso05 <shristisoni051199@gmail.com>
2024-01-14 23:05:40 +0530

    set_up_apis

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,26 +1,19 @@
 import cat1 from "../../../assets/category/cat-1.jpg";
-import cat2 from "../../../assets/category/cat-2.jpg";
-import cat3 from "../../../assets/category/cat-3.jpg";
-import cat4 from "../../../assets/category/cat-4.jpg";
+
 
 import "./Category.scss";
-const Category = () => {
-    return <div className="shop-by-category">
-        <div className="categories">
-            <div className="category">
-                <img src={cat1} alt=""  />
-            </div>
-            <div className="category">
-                <img src={cat2} alt="" />
-            </div>
-            <div className="category">
-                <img src={cat3} alt="" />
-            </div>
-            <div className="category">
-                <img src={cat4} alt="" />
+const Category = ({categories}) => {
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+                {categories?.data.map((items) =>
+                <div className="category">
+                    <img src={process.env.REACT_APP_DEV_URL + items?.attributes?.image?.data?.attribiutes?.url} alt="new"  />
+                </div>
+            )}
             </div>
         </div>
-    </div>;
+    );
 };
 
 export default Category;
--- src/utils/context.js
+++ src/utils/context.js
@@ -2,7 +2,20 @@ import { createContext, useState } from "react";
 export const Context = createContext();
 
 const AppContext = ({ children })  => {
-    return<Context.Provider>{children}</Context.Provider>;
+    const [categories, setCategoires] = useState();
+    const [products, setProducts] = useState();
+    return (
+       <Context.Provider 
+         value={{
+            categories,
+            setCategoires,
+            products,
+            setProducts,
+        }}
+    >
+        {children}
+      </Context.Provider>
+    );
 };
 
 export default AppContext;
